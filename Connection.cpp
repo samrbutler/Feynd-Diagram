05fc8c04b504f58fe503cfd80b903753245c76f3
samrbutler <sam.r.butler@outlook.com>
2020-12-15 15:31:44 +0000

    Removed debug code

--- Connection.cpp
+++ Connection.cpp
@@ -1,7 +1,7 @@
 #include "Connection.h"
 
 //Given a diagram and interaction dictionaries, return a vector of all possible completed tree-level diagrams
-std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nto1, bool debug)
+std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nto1)
 {
 	/* ALGORITHM
 	*	> Check if the external particles can connect directly into a vertex with at least two active particles
@@ -24,16 +24,6 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 	//Store the current external vertices of this diagram
 	const std::vector<Particle>& externs{ diag.getExterns() };
 
-	//DEBUG: Print details about all the current external particles
-	if (debug) {
-		std::cout << "Current External Particles\n";
-		for (const Particle& part : externs) {
-			std::cout << part.getType() << " , " << part.isActive() << " | ";
-		}
-		std::cout << '\n';
-		std::cout << "---------------------------------------------\n";
-	}
-
 	//Store the number of external particles
 	size_t s{ externs.size() };
 
@@ -42,24 +32,12 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 
 	//Check to see if we can form a vertex
 	if (diag.isVertex(nto0)) {
-		//DEBUG: Declare this to be a vertex
-		if (debug) {
-			std::cout << "IS VERTEX : DONE\n";
-			std::cout << "=============================================\n";
-		}
-
 		//Add a diagram consisting of the external points and a vertex connecting them
 		returnvec.push_back(Diagram(externs, Vertex(externs)));
 	}
 
 	//If we don't have a vertex but there are now two or fewer particles left, this process has failed, so return an empty vector
 	else if (s <= 2) {
-		//DEBUG: Declare a failure
-		if (debug) {
-			std::cout << "FAILED : RETURNING\n";
-			std::cout << "=============================================\n";
-		}
-
 		//Return an empty vector
 		return {};
 	}
@@ -70,63 +48,13 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 	//Get all possible groupings
 	listofpairedgroupings groupinglist{ getGroupings(input) };
 
-	//DEBUG : Display all groupings
-	if (debug) {
-		std::cout << "FULL GROUPING LIST\n";
-		for (const pairedgrouping& pg : groupinglist) {
-			for (const std::vector<Particle>& group : pg.first) {
-				for (const Particle& part : group) {
-					std::cout << part.getType() << ',';
-				}
-				std::cout << '|';
-			}
-			for (const Particle& part : pg.second) {
-				std::cout << part.getType() << ',';
-			}
-			std::cout << '\n';
-		}
-		std::cout << "---------------------------------------------\n";
-	}
-
 	//For each grouping in the list...
 	for (pairedgrouping& grp : groupinglist) {
 		//...get a list of possible new products for this group
 		listofproducts prodlist{ getNewExterns(grp, nto1) };
-		//DEBUG : Display the current grouping and what it will reduce to
-		if (debug) {
-			std::cout << "Current Grouping: \n\t";
-			for (const std::vector<Particle>& group : grp.first) {
-				for (const Particle& part : group) {
-					std::cout << part.getType() << ',';
-				}
-				std::cout << "| ";
-			}
-			std::cout << " |X| ";
-			for (const Particle& part : grp.second) {
-				std::cout << part.getType() << ',';
-			}
-			std::cout << '\n';
-			std::cout << "Product lists: \n";
-			for (const std::vector<P>& combo : prodlist) {
-				std::cout << '\t';
-				for (const P group : combo) {
-					std::cout << group << "| ";
-				}
-				std::cout << '\n';
-			}
-		}
-
+		
 		//Skip this grouping if it doesn't produce any valid interactions
-		if ((prodlist.size() == 0 || prodlist.size() == 1) && (prodlist[0].size() == 0)) {
-			//DEBUG : Declare the grouping to be invalid
-			if (debug) {
-				std::cout << "GROUPING INVALID : CONTINUING\n";
-				std::cout << "---------------------------------------------\n";
-			}
-
-			//Skip
-			continue;
-		}
+		if ((prodlist.size() == 0 || prodlist.size() == 1) && (prodlist[0].size() == 0)) continue;
 
 		//Go through each possible interaction product
 		for (size_t j{}; j < prodlist.size(); ++j) {
@@ -144,15 +72,6 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 				particlestoadd.push_back(p_to_add);
 				vertexparts.push_back(p_to_add.toggleAntiPart());
 
-				//DEBUG: Print out the vertex that's going to be added
-				if (debug) {
-					std::cout << "Adding a vertex:\n\t";
-					for (size_t k{}; k < vertexparts.size(); ++k) {
-						std::cout << vertexparts[k].getType() << ", " << vertexparts[k].getID() << " | ";
-					}
-					std::cout << '\n';
-				}
-
 				//Create and add the vertex to the current diagram
 				verticestoadd.push_back(Vertex(vertexparts));
 			}
@@ -162,29 +81,14 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 				particlestoadd.push_back(ungrouped.setActive(false));
 			}
 
-			//DEBUG: Display the number of new particles
-			if (debug) {
-				std::cout << "Adding " << particlestoadd.size() << " new particles to a subdiagram.\nInitiating recursion.\n";
-				std::cout << "---------------------------------------------\n";
-			}
-
 			//At this point we are ready to create a new subdiagram
 			Diagram subdiag{ particlestoadd };
 
 			//Initiate a recursion by connecting the subdiagram
-			std::vector<Diagram> connectedsds{ connect(subdiag, nto0, nto1, debug) };
+			std::vector<Diagram> connectedsds{ connect(subdiag, nto0, nto1) };
 
 			//If we got nothing back, this grouping has failed
-			if (connectedsds.size() == 0) {
-				//DEBUG: Declare a failure to connect the subdiagram
-				if (debug) {
-					std::cout << "SUBDIAGRAM FAILED TO CONNECT\n";
-					std::cout << "---------------------------------------------\n";
-				}
-
-				//Skip
-				continue;
-			}
+			if (connectedsds.size() == 0) continue;
 
 			//If we got diagrams back, for each subdiagram create a new diagram which combines the vertices... 
 			//...and external particles of the current diagram with the vertices of the subdiagram
@@ -197,13 +101,6 @@ std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0, const n1dict& nt
 				returnvec.push_back(newdiag);
 			}
 		}
-
-	}
-
-	//DEBUG: Declare a return up the chain recursion
-	if (debug) {
-		std::cout << "RETURNING\n";
-		std::cout << "=============================================\n";
 	}
 
 	//We're out of options so jump back up the chain
--- Connection.h
+++ Connection.h
@@ -10,4 +10,4 @@
 #include <utility>
 #include <vector>
 
-std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0 = Model::n_to_0, const n1dict& nto1 = Model::n_to_1, const bool debug = false);
+std::vector<Diagram> connect(Diagram& diag, const n0dict& nto0 = Model::n_to_0, const n1dict& nto1 = Model::n_to_1);
